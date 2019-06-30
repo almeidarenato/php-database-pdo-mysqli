57cf599c0b2c15db4602ee3336bf208f662eaf75
almeidarenato <renatoam@ymail.com>
2019-06-30 10:42:25 -0300

    troca do bulma pelo water.css e inclusao do pdo

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -4,5 +4,12 @@ $dbUserName = 'root';
 $dbPassword = 'root';
 $dbName = 'loginsystem';
 
+// utilizando prepared statement
+//$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
 
-$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+//utilizando PDO
+try {
+  $pdo = new PDO("mysql:host=$dbServerName;dbname=$dbName", $dbUserName, $dbPassword);
+} catch (PDOException $err) {
+  echo 'Erro de Conexão. Código : ' . $err->getCode();
+}
--- index.php
+++ index.php
@@ -10,48 +10,89 @@ include_once 'includes/dbh-inc.php';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>PHP Connection Test</title>
-  <link rel="stylesheet" href="bulma/css/bulma.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css">
 </head>
 
 <body>
-  <section class="section">
-    <div class="container">
-      <h1 class='title'>Teste de conexão com base de dados em PHP</h1>
-      <h2 class='subtitle'> Utilizando Prepared Statements </h2>
+  <section>
+    <div>
+      <h1>Teste de conexão com base de dados em PHP</h1>
+      <h2> Utilizando Prepared Statements </h2>
 
       <?php
       $sql = 'SELECT * FROM users';
       $result = mysqli_query($conn, $sql);
       $resultCheck = mysqli_num_rows($result);
-      ?>
-      <table class=" table ">
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>Login</th>
-            <th>Acesso</th>
-            <th>Ativo</th>
-          </tr>
-        </thead>
-        <?php
-        // verifica se o numero de linhas retornado é maior que 1 
-        if ($resultCheck == 0) {
-          echo 'não há linhas';
-        } else
-          while ($row = mysqli_fetch_assoc($result)) {
-
-            ?>
-          <tbody>
+      if ($resultCheck == 0) {
+        echo '<mark> sem resultados </mark>';
+      } else {
+        ?>
+        <table>
+          <thead>
             <tr>
-              <td scope=" row "><?php echo $row['id'] ?></td>
-              <td><?php echo $row['login'] ?></td>
-              <td><?php echo $row['nivel_acesso'] ?></td>
-              <td><?php echo $row['ativo'] ?></td>
+              <th>id</th>
+              <th>Login</th>
+              <th>Acesso</th>
+              <th>Ativo</th>
             </tr>
-          </tbody>
+          </thead>
+          <?php
+          // verifica se o numero de linhas retornado é maior que 1 
 
-        <?php } ?>
+          while ($row = mysqli_fetch_assoc($result)) {
+            ?>
+            <tbody>
+              <tr>
+                <td scope=" row "><?php echo $row['id'] ?></td>
+                <td><?php echo $row['login'] ?></td>
+                <td><?php echo $row['nivel_acesso'] ?></td>
+                <td><?php echo $row['ativo'] ?></td>
+              </tr>
+            </tbody>
+          <?php }
+        }
+        ?>
       </table>
+    </div>
+  </section>
+  <section>
+    <div>
+      <h2> Utilizando PDO </h2>
+      <table>
+        <?php
+        $query = 'SELECT * FROM users WHERE ativo=:ativo';
+        $parametro = 1;
+        $safeSearch = $pdo->prepare($query);
+        $safeSearch->bindValue(':ativo', $parametro, PDO::PARAM_BOOL);
+        $safeSearch->execute();
+        if ($safeSearch->rowCount() == 0) {
+          echo '<mark> sem resultados </mark>';
+        } else { ?>
+          <table>
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>Login</th>
+                <th>Acesso</th>
+                <th>Ativo</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+              $result = $safeSearch->fetchAll();
+              foreach ($result as $arrays) {
+                ?>
+
+                <tr>
+                  <td><?php echo $arrays['id']; ?></td>
+                  <td><?php echo $arrays['login']; ?></td>
+                  <td><?php echo $arrays['nivel_acesso']; ?></td>
+                  <td><?php echo $arrays['ativo']; ?></td>
+                </tr>
+              <?php }
+            } ?>
+          </tbody>
+        </table>
     </div>
   </section>
 
